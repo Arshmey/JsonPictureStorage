c6c0628f9c7cd51e4fc7c1f7a3c3e29bae7c6bac
dimag <dimag@ARSHMEY>
2025-03-21 22:46:50 +0300

    Add project files.

---
+++ JsonPictureStorage/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers.Text;
+using System.Text.Json;
+
+namespace JsonPictureStorage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream file = new FileStream(@"exampler.jpg", FileMode.Open))
+            using (MemoryStream data = new MemoryStream())
+            {
+                file.CopyTo(data);
+                File.WriteAllText(@"Done.txt", JsonSerializer.Serialize(data.ToArray()));
+            }
+        }
+    }
+}
